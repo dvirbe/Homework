c5e5efaa1f1880dd7a941fcc578f0448e03dfde3
Dvir <dvirbe@gmail.com>
2022-11-17 17:50:10 +0200

    fix negative numbers

--- Exercise6.java
+++ Exercise6.java
@@ -4,15 +4,27 @@ public class Exercise6 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter a number to check sum of digits :");
+        System.out.println("enter a whole number to check sum of digits :");
         int number = scanner.nextInt();
         int sum= 0;
-    while (number>0){
-        int lastDigit = number%10;
-        sum+=lastDigit;
-        number = number/10;
+        if (number<0){
+            while (number<0){
+            int lastDigit = number%10;
+            sum+=lastDigit;
+            number = number/10;
         }
-        System.out.println("sum of digits is : "+sum);
+            sum*=-1;
+            System.out.println("sum of digits is : "+sum);
+
+
+        }else {
+            while (number > 0) {
+                int lastDigit = number % 10;
+                sum += lastDigit;
+                number = number / 10;
+            }
+            System.out.println("sum of digits is : " + sum);
+        }
     }
 }
 
